2c771fd95084f2cd9f1fc20275b7e4a2937b8246
HoangNam <1660359@student.hcmus.edu.vn>
2019-06-01 04:29:35 +0700

    change page about part 2
    change page about part 2

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,68 +9,64 @@ export default class About extends PureComponent {
     render() {
         return (
             <div className="about-team">
-                <h1 className="about-team-title">BIẾT HƠN VỀ CHÚNG TÔI</h1>
+                <h1 className="text-center">BIẾT HƠN VỀ CHÚNG TÔI</h1>
                 <div className="container">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <div className="card about-img">
-                                <div className="imgs">
-                                    <img className="card-img-top" src={Avart1} alt="anh1" />
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title text-center">Trần Phạm Hoàng Nam</h5>
-                                    <p className="card-text text-center">MSSV - 1660359</p>
-                                    <div className="social">
-                                        <i className="fa fa-facebook" />
-                                        <i className="fa fa-twitter" />
-                                        <i className="fa fa-pinterest-p" />
-                                        <i className="fa fa-linkedin" />
-                                    </div>
-                                </div>
+                               
+                    <div className="member-items">
+                        <div className="members">
+                            <div className="img-menbers">
+                                <img className="" src={Avart1} alt="anh1" />
                             </div>
-                        </div>
-
-                         <div className="col-md-4">
-                            <div className="card about-img">
-                                <div className="imgs">
-                                    <div className="nenxam"></div>
-                                    <img className="card-img-top" src={Avart1} alt="anh1" />
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title text-center">Trần Phạm Hoàng Nam</h5>
-                                    <p className="card-text text-center">MSSV - 1660359</p>
-                                    <div className="social">
-                                        <i className="fa fa-facebook" />
-                                        <i className="fa fa-twitter" />
-                                        <i className="fa fa-pinterest-p" />
-                                        <i className="fa fa-linkedin" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                         <div className="col-md-4">
-                            <div className="card about-img">
-                                <div className="imgs">
-                                    <div className="nenxam"></div>
-                                    <img className="card-img-top" src={Avart1} alt="anh1" />
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title text-center">Trần Phạm Hoàng Nam</h5>
-                                    <p className="card-text text-center">MSSV - 1660359</p>
-                                    <div className="social">
-                                        <i className="fa fa-facebook" />
-                                        <i className="fa fa-twitter" />
-                                        <i className="fa fa-pinterest-p" />
-                                        <i className="fa fa-linkedin" />
-                                    </div>
+                            <div className="content-member">
+                                <h5 className="text-center">Trần Phạm Hoàng Nam</h5>
+                                <p className="card-text text-center">MSSV - 1660359</p>
+                                <div className="social">
+                                    <i className="fa fa-facebook" />
+                                    <i className="fa fa-twitter" />
+                                    <i className="fa fa-pinterest-p" />
+                                    <i className="fa fa-linkedin" />
                                 </div>
                             </div>
                         </div>
                     </div>
+
+                    <div className="member-items">
+                        <div className="members">
+                            <div className="img-menbers">
+                                <img className="" src={Avart1} alt="anh1" />
+                            </div>
+                            <div className="content-member">
+                                <h5 className="text-center">Trần Phạm Hoàng Nam</h5>
+                                <p className="card-text text-center">MSSV - 1660359</p>
+                                <div className="social">
+                                    <i className="fa fa-facebook" />
+                                    <i className="fa fa-twitter" />
+                                    <i className="fa fa-pinterest-p" />
+                                    <i className="fa fa-linkedin" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="member-items">
+                        <div className="members">
+                            <div className="img-menbers">
+                                <img className="" src={Avart1} alt="anh1" />
+                            </div>
+                            <div className="content-member">
+                                <h5 className="text-center">Trần Phạm Hoàng Nam</h5>
+                                <p className="card-text text-center">MSSV - 1660359</p>
+                                <div className="social">
+                                    <i className="fa fa-facebook" />
+                                    <i className="fa fa-twitter" />
+                                    <i className="fa fa-pinterest-p" />
+                                    <i className="fa fa-linkedin" />
+                                </div>
+                            </div>
+                        </div>
+                </div>
                 </div>
             </div>
-
         );
     }
 }
