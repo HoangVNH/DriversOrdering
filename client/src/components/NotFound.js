32f13fbe42c5a48a75022cc1d0310eb063356f2b
HoangNam <1660359@student.hcmus.edu.vn>
2019-06-01 00:40:32 +0700

    create page not found
    create page not found

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,13 +1,22 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-unused-expressions */
 import React, { PureComponent } from 'react';
+import { NavLink } from 'react-router-dom';
 
+import '../css/not_found/notfound.css';
 export default class About extends PureComponent {
 
     render() {
         return (
-           <div>
-				<h1>404 - Không tìm thấy trang</h1> 
+           <div className="found">
+                <section className="error-container">
+                    <span>4</span>
+                    <span><span className="screen-reader-text">0</span></span>
+                    <span>4</span>
+                </section>
+                <div className="link-container">
+                    <NavLink to="/" className="more-link">Trở Về Trang Chủ</NavLink>
+                </div>
            </div>
         );
     }
