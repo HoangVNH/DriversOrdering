fbe21e5b3471e7943e810bd9413f125bba297af7
HoangNam <1660359@student.hcmus.edu.vn>
2019-06-01 00:57:19 +0700

    change height for maps

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,35 +1,73 @@
-import React, { PureComponent } from 'react';
-import { default as GLOBAL } from './Constants';
-import { default as API_URL } from '../apis/APICommonService';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
-import { compose, withProps } from "recompose";
+import React, {
+    PureComponent
+} from 'react';
+import {
+    default as GLOBAL
+} from './Constants';
+import {
+    default as API_URL
+} from '../apis/APICommonService';
+import {
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    Marker
+} from "react-google-maps";
+import {
+    compose,
+    withProps
+} from "recompose";
 
 import '../js/nav';
 
 const MyMapComponent = compose(
     withProps({
         googleMapURL: GLOBAL.SERVER_HOST + API_URL.MAP,
-        loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `850px` }} />,
-        mapElement: <div style={{ height: `100%` }} />,
+        loadingElement: < div style = {
+            {
+                height: `100%`
+            }
+        }
+        />,
+        containerElement: < div style = {
+            {
+                height: `100%`
+            }
+        }
+        />,
+        mapElement: < div style = {
+            {
+                height: `100%`
+            }
+        }
+        />,
     }),
     withScriptjs,
     withGoogleMap
-)((props) => 
-    <GoogleMap
-        defaultZoom = {19}
-        defaultCenter = {{ lat: GLOBAL.LATLNG.lat, lng: GLOBAL.LATLNG.lng }}
-    ></GoogleMap>
+)((props) =>
+    <
+    GoogleMap defaultZoom = {
+        19
+    }
+    defaultCenter = {
+        {
+            lat: GLOBAL.LATLNG.lat,
+            lng: GLOBAL.LATLNG.lng
+        }
+    } >
+    < /GoogleMap>
 )
 
 export default class Map extends PureComponent {
 
     render() {
-        return (
-            <main className="">
-                <MyMapComponent />  
-            </main>
-           
+        return ( <
+            main className = "" >
+            <
+            MyMapComponent / >
+            <
+            /main>
+
         );
     }
 }
