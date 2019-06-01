44ca4e1f2b662b919f14f51afee2d7585042ed48
HoangNam <1660359@student.hcmus.edu.vn>
2019-06-02 01:03:28 +0700

    change form edit info part 2

--- client/src/components/Info.js
+++ client/src/components/Info.js
@@ -1,62 +1,101 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-expressions */
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
 
 import '../css/info_user/info.css';
 import '../css/lightBox/lightbox.css';
 import '../js/lightbox-plus-jquery';
 
-class Info extends Component {
+export default class Edit_Info extends Component {
+
     render() {
         return (
-
-            <div className="info-user">
-                <div className="info-title">
-                    <h3>Thông tin cá nhân</h3>
-                    <div className="gach"></div>
+            <div className="info">
+            <header className="jumbotron">
+                <h1 className="text-center display-5" id="title"> CHỈNH SỬA THÔNG TIN CÁ NHÂN </h1>
+                <div className="container">
+                    <p id="description" className="lead">
+                    <strong> Hướng dẫn: </strong> Thay đổi thông tin cá nhân của bạn một cách hợp lý và cuối cùng nhấn chọn lưu thông tin. Nếu bạn muốn muốn thay đổi hình ảnh hình ảnh thì hãy chọn ảnh để thay đổi</p>
                 </div>
-                <div className="info-content">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-12 mt-5">
-                                <table className="table table-striped table-inverse">
-                                    <thead>
-                                        <tr>
-                                            <th>Họ Tên</th>
-                                            <th>Số Điện Thoại</th>
-                                            <th>Địa Chỉ</th>
-                                            <th className="text-center">Thông Tin Xe</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>Trần Phạm Hoàng Nam</td>
-                                                <td>0123 456 789</td>
-                                                <td>227 Nguyễn Văn Cừ, Q5</td>
-                                                <td className="text-center">
-                                                    <a style={{margin: '0 10px'}} className="example-image-link" href="http://lokeshdhakar.com/projects/lightbox2/images/image-1.jpg" data-lightbox="example-1">
-                                                        <img width={130} className="example-image" src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-1.jpg" alt="anh22" />
-                                                    </a>
-                                                    <a style={{margin: '0 10px'}} className="example-image-link" href="http://lokeshdhakar.com/projects/lightbox2/images/image-1.jpg" data-lightbox="example-1">
-                                                        <img width={130} className="example-image" src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-1.jpg" alt="anh22" />
-                                                    </a>
-                                                    <a style={{margin: '0 10px'}} className="example-image-link" href="http://lokeshdhakar.com/projects/lightbox2/images/image-1.jpg" data-lightbox="example-1">
-                                                        <img width={130} className="example-image" src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-1.jpg" alt="anh22" />
-                                                    </a>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                </table>
-                                <NavLink to="/info/edit_info" className="btn btn-danger pull-right mr-4" id="submit">
-                                    <i className="fa fa-arrow-right"></i> Sửa Thông Tin
-                                </NavLink>
+            </header>
+
+
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <form id="survey-form">
+                            <h5 className="title"> Chỉnh Sửa Thông Tin Của Bạn </h5>
+
+                            <div className="form-group d-flex">
+                            <input className="form-control" type="text" id="name" placeholder="Họ tên" />
                             </div>
+
+                            <div className="form-group d-flex">
+                            <input className="form-control" type="text" id="lastName" placeholder="Số điện thoại" />
+                            </div>
+
+                            <div className="form-group d-flex">
+                            <input className="form-control" type="text" id="lastName" placeholder="Địa chỉ" />
+                            </div>
+
+                            <div className="form-group d-flex">
+                            <input className="form-control" type="password" id="password" placeholder="Nhập mật khẩu mới" />
+                            </div>
+                            
+                            <div className="form-group d-flex">
+                            <input className="form-control" type="password" id="repassword" placeholder="Nhập lại mật khẩu" />
+                            </div>                            
+
+                            <button className="btn btn-success pull-right" id="submit">
+                                <i className="fa fa-check"></i> Lưu Thông Tin
+                            </button>
+
+                        </form>
+
+                    </div>
+                    <div className="col-md-4">
+                        <div className="form-hinhanh">
+                            <h5 className="title">Thay đổi hình ảnh</h5>
+
+                            <form>
+                                <a style={{float: 'left'}} className="example-image-link" href="http://lokeshdhakar.com/projects/lightbox2/images/image-1.jpg" data-lightbox="example-1">
+                                    <img width={100} className="example-image" src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-1.jpg" alt="anh22" />
+                                </a>
+                                <p>Chân dung</p>
+                                <button className="btn btn-success pull-right thay-doi">Thay đổi <i className="fa fa-check"></i></button>
+                                <div className="form-group d-flex my-2">
+                                    <input className="form-control" type="file" id="" placeholder="Nhập lại mật khẩu" />                                
+                                </div>
+                            </form>
+                             
+
+                            <form>
+                                <a style={{float: 'left'}} className="example-image-link" href="http://lokeshdhakar.com/projects/lightbox2/images/image-1.jpg" data-lightbox="example-1">
+                                    <img width={100} className="example-image" src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-1.jpg" alt="anh22" />
+                                </a>
+                                <p>Biển số</p>
+                                <button className="btn btn-success pull-right thay-doi">Thay đổi <i className="fa fa-check"></i></button>
+                                <div className="form-group d-flex my-2">
+                                    <input className="form-control" type="file" id="" placeholder="Nhập lại mật khẩu" />                                
+                                </div>
+                            </form>
+
+                            <form>
+                                <a style={{float: 'left'}} className="example-image-link" href="http://lokeshdhakar.com/projects/lightbox2/images/image-1.jpg" data-lightbox="example-1">
+                                    <img width={100} className="example-image" src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-1.jpg" alt="anh22" />
+                                </a>
+                                <p>Hình xe</p>
+                                <button className="btn btn-success pull-right thay-doi">Thay đổi  <i className="fa fa-check"></i></button>
+                                <div className="form-group d-flex my-2">
+                                    <input className="form-control" type="file" id="" placeholder="Nhập lại mật khẩu" />                                
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-            </div>
 
+            </div>
+        </div>
         );
     }
 }
-
-export default Info;
