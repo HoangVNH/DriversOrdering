b7f146307a8f6992bf59e693027bb7332a39053e
Hoang Vu <hoangvu0897@gmail.com>
2019-06-03 00:34:28 +0700

    Fix Grammar

--- client/src/components/Constants.js
+++ client/src/components/Constants.js
@@ -1,10 +1,10 @@
-const UNIVERSITY_SCIENCE_POSTION = {
+const UNIVERSITY_SCIENCE_POSITION = {
     lat: 10.7627863,
     lng: 106.6815868
 };
 
 export default module = {
-     LATLNG: UNIVERSITY_SCIENCE_POSTION,
+     LATLNG: UNIVERSITY_SCIENCE_POSITION,
      SERVER_HOST: "https://maps.googleapis.com/maps/api",
      API_KEY: "AIzaSyDgzrSsa_eHlXK8ESd84WXYfMK71N45v14"
 };
