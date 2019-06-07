0a723b21b0d8acf46e62baefcf3e35b3374653d1
Hoang Vu <hoangvu0897@gmail.com>
2019-06-08 00:05:43 +0700

    Changed Map UI

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,72 +1,19 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 import { default as GLOBAL } from "./Constants";
-import { default as API_URL } from "../apis/APICommonService";
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
-import { compose, withProps } from "recompose";
-
 import "../js/nav";
+import AsyncMap from './AsyncMap';
 
-const MyMapComponent = compose(
-  withProps({
-    googleMapURL: GLOBAL.SERVER_HOST + API_URL.MAP,
-    loadingElement: (
-      <div
-        style={{  height: `100%`  }}
-      />
-    ),
-    containerElement: (
-      <div
-        style={{  height: `100%`  }}
-      />
-    ),
-    mapElement: (
-      <div
-        style={{  height: `100%`  }}
-      />
-    )
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props => (
-  <GoogleMap
-    defaultZoom={19}
-    defaultOptions={{
-      mapTypeControl: false,
-      streetViewControl: false,
-      fullscreenControl: false,
-      scaleControl: false,
-      rotateControl: false,
-      zoomControl: false,
-      styles: [
-          {
-            featureType: 'poi.business',
-            stylers: [{visibility: 'off'}]
-          },
-          {
-            featureType: 'transit',
-            elementType: 'labels.icon',
-            stylers: [{visibility: 'off'}]
-          }
-        ]
-    }}
-    defaultCenter={{  lat: GLOBAL.LATLNG.lat, lng: GLOBAL.LATLNG.lng}}>
-    <Marker 
-      position= {{  lat: GLOBAL.LATLNG.lat, lng: GLOBAL.LATLNG.lng  }}
-    />
-  </GoogleMap>
-));
+export default class Map extends Component {
 
-export default class Map extends PureComponent {
-  render() {
-    return (
-      <main className="">
-        <MyMapComponent />
-      </main>
-    );
-  }
+	render() {
+		return (
+      <div style={{ margin: `100px` }}>
+        <AsyncMap
+          google={this.props.google}
+          center={{ lat: GLOBAL.LATLNG.lat, lng: GLOBAL.LATLNG.lng }}
+        />
+      </div>
+        
+		);
+	}
 }
