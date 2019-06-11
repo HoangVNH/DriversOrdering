26f8865519ed651534a4b5d310f20770f0e99e60
John Smith <wacui113@gmail.com>
2019-06-11 22:55:15 +0700

    Edit About Us

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,9 @@
 import React, { PureComponent } from 'react';
 import '../css/not_found_about/about.css';
 
-import Avart1 from '../images/avatar_1.jpg';
+import Avart1 from '../images/tphn.jpg';
+import Avart2 from '../images/vnhh.jpg';
+import Avart3 from '../images/cct.jpg';
 export default class About extends PureComponent {
 
     render() {
@@ -33,11 +35,11 @@ export default class About extends PureComponent {
                     <div className="member-items">
                         <div className="members">
                             <div className="img-menbers">
-                                <img className="" src={Avart1} alt="anh1" />
+                                <img className="" src={Avart2} alt="anh2" />
                             </div>
                             <div className="content-member">
-                                <h5 className="text-center">Trần Phạm Hoàng Nam</h5>
-                                <p className="card-text text-center">MSSV - 1660359</p>
+                                <h5 className="text-center">Vũ Nguyễn Huy Hoàng</h5>
+                                <p className="card-text text-center">MSSV - 1660214</p>
                                 <div className="social">
                                     <i className="fa fa-facebook" />
                                     <i className="fa fa-twitter" />
@@ -51,11 +53,11 @@ export default class About extends PureComponent {
                     <div className="member-items">
                         <div className="members">
                             <div className="img-menbers">
-                                <img className="" src={Avart1} alt="anh1" />
+                                <img className="" src={Avart3} alt="anh3" />
                             </div>
                             <div className="content-member">
-                                <h5 className="text-center">Trần Phạm Hoàng Nam</h5>
-                                <p className="card-text text-center">MSSV - 1660359</p>
+                                <h5 className="text-center">Hồ Kiếm Hùng</h5>
+                                <p className="card-text text-center">MSSV - 1660227</p>
                                 <div className="social">
                                     <i className="fa fa-facebook" />
                                     <i className="fa fa-twitter" />
