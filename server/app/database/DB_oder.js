9ede5438492f9e50d42b5dee17fc11cf7c62c734
HoangNam <1660359@student.hcmus.edu.vn>
2019-06-15 13:40:00 +0700

    change table order
    change table order

--- server/app/database/DB_oder.js
+++ server/app/database/DB_oder.js
@@ -1,27 +1,27 @@
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-// // Customer Schema 
-// var orderSchema = mongoose.Schema({
-//     idDriver: {
-//         type: String,
-//         require: true
-//     },
-//     customerMobileNum: {
-//         type: String,
-//         require: true
-//     },
-//     price: {
-//         type: Int32Array,
-//         require: true
-//     },
-//     status: {
-//         type: Boolean,
-//         default: false
-//     },
-//     created_at: {
-//         type: Date,
-//         default: Date.now()
-//     }
-// }, { collection: "order" });
+// Customer Schema 
+var orderSchema = mongoose.Schema({
+    idDriver: {
+        type: String,
+        require: true
+    },
+    customerMobileNum: {
+        type: String,
+        require: true
+    },
+    price: {
+        type: String,
+        require: true
+    },
+    status: {
+        type: Boolean,
+        default: false
+    },
+    created_at: {
+        type: Date,
+        default: Date.now()
+    }
+}, { collection: "order" });
 
-// module.exports = mongoose.model('orderSchema', orderSchema);
+module.exports = mongoose.model('orderSchema', orderSchema);
